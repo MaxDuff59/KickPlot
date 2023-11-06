8f09969aa888af815fe273cfac140ff332bd37b1
MaxDuff59 <78801110+MaxDuff59@users.noreply.github.com>
2023-11-06 12:19:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,35 +6,51 @@ import fonction
 st.set_page_config(layout="wide")
 
 def main():
-    
-    st.title("Racing 92 - Graphiques KICKING 🔵⚪️")
-    
-    uploaded_file = st.file_uploader("Importer le fichier CSV", type=["csv"])
-    
-    if uploaded_file is not None:
 
-        st.write("File Uploaded Successfully!")
+    tab1, tab2 = st.tabs(["Kicking Graphs", "Dog", "Owl"])
+
+    with tab1:
         
-        df = pd.read_csv(uploaded_file)
-        joueurs_racing = list(df[df.Row == "17.JAP Racing 92"].Joueurs.unique())
-
-        player_inputs = {}
-    
-        for player in joueurs_racing:
-
-            player_input = st.text_input(f"N° de {player} : ")
-            player_inputs[player] = player_input
+        st.title("Racing 92 - Graphiques KICKING 🔵⚪️")
         
-        st.write("Player Inputs:")
-        st.write(player_inputs)
+        uploaded_file = st.file_uploader("Importer le fichier CSV", type=["csv"])
+        
+        if uploaded_file is not None:
+    
+            st.write("File Uploaded Successfully!")
+            
+            df = pd.read_csv(uploaded_file)
+            joueurs_racing = list(df[df.Row == "17.JAP Racing 92"].Joueurs.unique())
+    
+            player_inputs = {}
+        
+            for player in joueurs_racing:
+    
+                player_input = st.text_input(f"N° de {player} : ")
+                player_inputs[player] = player_input
+            
+            st.write("Player Inputs:")
+            st.write(player_inputs)
+    
+            if st.button("Process Images"):
+    
+                img = fonction.kicking_plot(df,player_inputs)
+                st.image(img)
+    
+                img = fonction.kicking_plot_adv(df)
+                st.image(img)
 
-        if st.button("Process Images"):
+    with tab2:
 
-            img = fonction.kicking_plot(df,player_inputs)
-            st.image(img)
+        st.title('Collective Experience')
 
-            img = fonction.kicking_plot_adv(df)
-            st.image(img)
+        url = st.text_input('URL du match : ', '')
+
+        if st.button('Téléchargement des données'):
+            
+            df_compo()
+
+
 
 if __name__ == "__main__":
     main()
